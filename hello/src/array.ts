cc19271f0d43d0992a45408d942121da785131ec
Emam saimon <likonsaimon123@gmail.com>
2025-10-01 21:31:24 +0600

    Add an Array

---
+++ hello/src/array.ts
@@ -0,0 +1,8 @@
+// const names: Array<string> = ["alice ", "Bd", "dfd"];
+
+// type 2
+const num: number[] = [1, 2, 3, 4];
+const name: string[] = ["Emam", "Hossain", "saimon"];
+console.log(num);
+console.log(name);
+

a191bd885c524ec8864a7a9d353c355e13818dac
Emam saimon <likonsaimon123@gmail.com>
2025-10-02 00:06:36 +0600

    Add a new file named touple.ts

---
+++ hello/src/touple.ts
@@ -0,0 +1,12 @@
+const emp: string[] = ['Imam', 'Hossain', 'Saimon'];
+const empData: [string, number, boolean, number] = ['Saimon', 20, true, 20000];
+
+console.log(empData);
+
+//const empData: readonly[string, number, boolean, number] = ['Saimon', 20, true, 20000];  -----> readonly korle empData.push() korte parbe na!!!
+
+// run this code:
+// ✅node dist/touple.js
+
+//watch this file-->
+// tsc --watch
